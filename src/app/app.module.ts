912fd9e4ddc8f0ad7428a00c5c45a4eb16dc6b46
nys950 <cm.myssolucionesintegrales@gmail.com>
2022-10-10 16:29:38 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './views/index/index.component';
+import { NavbarIndexComponent } from "./navegation/navbars/navbar-index/navbar-index.component";
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MegaMenuComponent } from './navegation/navbars/mega-menu/mega-menu.component';
+import { FooterComponent } from './navegation/footer/footer.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    NavbarIndexComponent,
+    CarouselComponent,
+    MegaMenuComponent,
+    FooterComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navegation/navbars/navbar-index/navbar-index.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-index',
+  templateUrl: './navbar-index.component.html',
+})
+export class NavbarIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
